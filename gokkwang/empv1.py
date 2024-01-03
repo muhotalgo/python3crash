2b46f81273159a530d2956b7e420466be3edfefc
YeEul, Kwon <gruuriii@gmail.com>
2024-01-03 10:56:09 +0900

    사원등록 프로그램 v1 - 사원정보 조회

--- gokkwang/empv1.py
+++ gokkwang/empv1.py
@@ -1,6 +1,23 @@
 import sys
 emps = []
 
+def load_employees():
+    print('프로그램을 초기화합니다...')
+    global emps
+    dicts = []
+
+    with open('employees.csv') as f:
+        datas = f.readlines()
+
+    for data in datas:
+        item = data.strip().split(',')
+        d = {'empno':item[0],'fname':item[1],'lname':item[2],'email':item[3],
+             'hdate':item[4],'jobid':item[5],'sal':item[6],'deptid':item[7]}
+        dicts.append(d)
+
+    emps = dicts
+
+    print('프로그램이 성공적으로 초기화되었습니다...')
 
 def show_menu():
     main_menu = '''
@@ -39,18 +56,22 @@ def save_employee(emp):
            f"{emp['sal']},{emp['deptid']}\n")
     with open('employees.csv', 'a') as f:
         f.write(row)
+    emps.append(emp)
 
 
 def add_employee():
     print('사원 정보를 등록합니다...')
     emp = input_employee()
     save_employee(emp)
-    return None
 
 
 def read_employee():
     print('모든 사원 정보를 조회합니다...')
-    return None
+    result = ''
+    for emp in emps:
+        result += (f"{emp['empno']}\t{emp['fname']}\t"
+                   f"{emp['jobid']}\t{emp['deptid']}\n")
+    print(result)
 
 
 def readone_employee():
@@ -72,8 +93,3 @@ def exit_program():
     print('프로그램을 종료합니다.!')
     sys.exit(0)
 
-
-def load_employees():
-    print('프로그램을 초기화합니다...')
-    print('프로그램이 성공적으로 초기화되었습니다...')
-    return None
