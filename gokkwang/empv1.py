5abb204e05ee9c1b9c5497cba0541ee4f98640bc
YeEul, Kwon <gruuriii@gmail.com>
2024-01-03 10:20:56 +0900

    사원등록 프로그램 v1 - 사원 추가

--- gokkwang/empv1.py
+++ gokkwang/empv1.py
@@ -19,8 +19,32 @@ def show_menu():
     menu = input('=> 메뉴를 선택하세요 : ')
     return menu
 
+
+def input_employee():
+    emp = {}
+    emp['empno'] = input('사원번호는? ')
+    emp['fname'] = input('이름은? ')
+    emp['lname'] = input('성은? ')
+    emp['email'] = input('이메일은? ')
+    emp['hdate'] = input('입사일은? ')
+    emp['jobid'] = input('직책은? ')
+    emp['sal'] = input('급여는? ')
+    emp['deptid'] = input('부서번호는? ')
+    return emp
+
+
+def save_employee(emp):
+    row = (f"{emp['empno']},{emp['fname']},{emp['lname']},"
+           f"{emp['email']},{emp['hdate']},{emp['jobid']},"
+           f"{emp['sal']},{emp['deptid']}\n")
+    with open('employees.csv', 'a') as f:
+        f.write(row)
+
+
 def add_employee():
     print('사원 정보를 등록합니다...')
+    emp = input_employee()
+    save_employee(emp)
     return None
 
 
@@ -47,3 +71,9 @@ def remove_employee():
 def exit_program():
     print('프로그램을 종료합니다.!')
     sys.exit(0)
+
+
+def load_employees():
+    print('프로그램을 초기화합니다...')
+    print('프로그램이 성공적으로 초기화되었습니다...')
+    return None
