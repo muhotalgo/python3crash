7d4d2fa292df07f67fca36c19d228d1470c41f3e
YeEul, Kwon <gruuriii@gmail.com>
2024-01-10 18:01:49 +0900

    도서관리 프로그램 v1 - 예외처리 코드 추가

--- gokkwang/BookService.py
+++ gokkwang/BookService.py
@@ -1,3 +1,4 @@
+import os
 import sys
 from gokkwang.Book import Book
 from gokkwang.BookDAO import BookDAO
@@ -34,18 +35,26 @@ class BookService:
 
     @staticmethod
     def __input_book():
-        bkname = input('도서명은? ')
-        author = input('도서 저자는? ')
-        publisher = input('도서 출판사는? ')
-        pubdate = input('도서 출간일은? ')
-        retail = int(input('도서 소매가는? '))
-        pctoff = int(input('도서 할인율은? '))
+        try:
+            bkname = input('도서명은? ')
+            author = input('도서 저자는? ')
+            publisher = input('도서 출판사는? ')
+            pubdate = input('도서 출간일은? ')
+            retail = int(input('도서 소매가는? '))
+            pctoff = int(input('도서 할인율은? '))
 
-        bk = Book(bkname, author, publisher, pubdate, retail, pctoff)
-        bk.price = bk.retail * (1 - (bk.pctoff / 100))
-        bk.mileage = bk.retail * (bk.pctoff / 100)
+            bk = Book(bkname, author, publisher, pubdate, retail, pctoff)
+            bk.price = bk.retail * (1 - (bk.pctoff / 100))
+            bk.mileage = bk.retail * (bk.pctoff / 100)
 
-        return bk
+            return bk
+        except:
+            print('BookService - input_book에서 오류 발생!!')
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            print('예외 내용 :', exc_obj)
+            print('예외 종류 :', exc_type.__name__)
+            print('예외 위치 :', fname, exc_tb.tb_lineno)
 
     @staticmethod
     # 도서 데이터 추가 (입력-처리-저장)
@@ -55,11 +64,19 @@ class BookService:
         :return:없음
         """
         print('도서 데이터 추가')
-        bk = BookService.__input_book()
-        print(bk)
+        try:
+            bk = BookService.__input_book()
+            print(bk)
 
-        rowcnt = BookDAO.insert_book(bk)
-        print(f'{rowcnt} 건의 도서데이터 등록됨!!')
+            rowcnt = BookDAO.insert_book(bk)
+            print(f'{rowcnt} 건의 도서데이터 등록됨!!')
+        except:
+            print('BookService - new_book에서 오류 발생!!')
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            print('예외 내용 :', exc_obj)
+            print('예외 종류 :', exc_type.__name__)
+            print('예외 위치 :', fname, exc_tb.tb_lineno)
 
     @staticmethod
     # 모든 도서 데이터 출력
