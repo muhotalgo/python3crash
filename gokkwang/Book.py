69f9e7e40306739f79b98e17f2e20c92c0080abc
YeEul, Kwon <gruuriii@gmail.com>
2024-01-10 11:48:23 +0900

    도서관리 프로그램 v1 - Book 클래스

--- gokkwang/Book.py
+++ gokkwang/Book.py
@@ -1,2 +1,22 @@
+# bkname, author, publisher, pubdate,
+# retail, price, pctoff, mileage
+
 class Book:
-    pass
+    def __init__(self, bkname, author, publisher, pubdate, retail, pctoff):
+        self.bkno = -1
+        self.bkname = bkname
+        self.author = author
+        self.publisher = publisher
+        self.pubdate = pubdate
+        self.retail = retail
+        self.price = -1
+        self.pctoff = pctoff
+        self.mileage = -1
+        self.regdate = -1
+
+    def __str__(self):
+        self.price = self.retail * (1 - (self.pctoff / 100))
+        self.mileage = self.retail * (self.pctoff / 100)
+        result = (f'{self.bkname} {self.author} {self.publisher} {self.pubdate} '
+                  f'{self.retail} {self.price} {self.pctoff} {self.mileage}')
+        return result
