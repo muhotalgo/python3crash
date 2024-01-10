b7874d1f79544f054ea654b1e25476b78ad75bfa
YeEul, Kwon <gruuriii@gmail.com>
2024-01-10 12:04:44 +0900

    도서관리 프로그램 v1 - Book 테이블 생성

---
+++ gokkwang/book.sql
@@ -0,0 +1,28 @@
+create table book
+(
+    bkno      int auto_increment,
+    bkname    varchar(100) not null,
+    author    varchar(100) not null,
+    publisher varchar(50)  not null,
+    pubdate   datetime     not null,
+    retail    int          not null,
+    price     int      default 0,
+    pctoff    int          not null,
+    mileage   int      default 0,
+    regdate   datetime default current_timestamp,
+    primary key (bkno)
+);
+
+insert into book (bkname, author, publisher, pubdate, retail, pctoff)
+values ('파이썬 기초','한빛미디어','한빛미디어','2024-01-01 00:00:000',35000,10);
+
+select bkname, bkname, author, publisher, price from book where bkno = 1;
+
+select * from book where bkno = 1;
+
+update book set bkname='',author='',publisher='',
+                pubdate='', retail='', pctoff=''
+where bkno = ?;
+
+delete from book
+where bkno = ?;
